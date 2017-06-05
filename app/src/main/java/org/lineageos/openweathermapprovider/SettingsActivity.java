3fe41331dcb56f236e89f5f988dcc81905fc28b6
Joey Rizzoli <joey@lineageos.org>
2017-06-05 20:55:35 +0200

    Add back key to actionbar
    Change-Id: Ia90237a923244996d77a9fe0d59b7280af7b882a
    Signed-off-by: Joey Rizzoli <joey@lineageos.org>

--- app/src/main/java/org/lineageos/openweathermapprovider/SettingsActivity.java
+++ app/src/main/java/org/lineageos/openweathermapprovider/SettingsActivity.java
@@ -16,6 +16,7 @@
 
 package org.lineageos.openweathermapprovider;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -24,7 +25,9 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class SettingsActivity extends Activity {
 
@@ -37,9 +40,24 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_back);
+        }
+
+
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new ServicePrefsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class ServicePrefsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
